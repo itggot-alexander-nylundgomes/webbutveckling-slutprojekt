4afec9a1c574614ff36ecf901dfbe91caf001281
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2019-05-02 15:43:32 +0200

    Keepin er goin

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,9 @@
 function dropdown_profile(){
     dropdown = document.querySelector(".drop-down-profile")
     dropdown.classList.toggle("a_dropdown_profile")
+
+    profilebox = document.querySelector(".profilebox")
+    profilebox.classList.toggle("a_profilebox")
+
+    
 }
